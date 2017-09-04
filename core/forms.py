b6d54f17667db9eb8ebf6665bc92c74edf0c5ed0
k <wer@w.pl>
2017-09-04 17:45:28 +0200

    update

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.exceptions import ValidationError
+from dateutil import relativedelta
 
 from .models import CustomUser
+
 
 class EmailAuthenticationForm(AuthenticationForm):
     username = forms.EmailField(widget=forms.EmailInput(attrs={'autofocus': True}))
@@ -16,12 +21,32 @@ class EmailAuthenticationForm(AuthenticationForm):
 class CustomCreationForm(UserCreationForm):
     date_of_birth = forms.DateField(required=True)
     gender = forms.ChoiceField(choices=(('m', 'Male'), ('f', 'Female'), ('o', 'Pre')), required=False)
-    accept_tos = forms.BooleanField(required=False, label="Accept TOS")
+    accept_tos = forms.BooleanField(required=True, label="Accept TOS")
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'password1', 'password2')
+        fields = ('email', 'password1', 'password2', 'date_of_birth')
+
+    def clean(self):
+        print(self.cleaned_data)
+        return self.cleaned_data
 
     def clean_date_of_birth(self):
-        print(self.cleaned_data)
+        date = self.cleaned_data.get('date_of_birth')
+        today = datetime.date(datetime.now())
+        if date > today:
+            raise ValidationError("You're born in the future congrats!", code='invalid')
+        if date < today - relativedelta.relativedelta(years=130):
+            raise ValidationError("You're 130 years+ old good for you!", code='invalid')
+        return date
 
+    def clean_accept_tos(self):
+        if not self.cleaned_data.get('accept_tos', None):
+            raise ValidationError('TOS must be accepted', code='invalid')
+        return self.cleaned_data['accept_tos']
+
+    def save(self, commit=False):
+        user = super(CustomCreationForm, self).save(commit)
+        user.date_of_birth = self.cleaned_data['date_of_birth']
+        user.save()
+        return user
